de5e27215a54b30cd2492b50523a8e696358df41
fuzz <fuzzzzzzzzzzzzzzzzzz@gmail.com>
2014-01-31 11:33:09 +0900

    Revert "tuna: Update to ro.ksm.default"
    This reverts commit 26c75d76b3d6a5f49ae559b87124fd4cd6689c57.

--- device.mk
+++ device.mk
@@ -93,6 +93,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	persist.sys.usb.config=mtp
 
+# Enable KSM by default
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	persist.sys.ksm=1
+
 # Torch
 PRODUCT_PACKAGES += \
         Torch
@@ -182,10 +186,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.hwui.disable_scissor_opt=true
-
-# Enable KSM by default
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.ksm.default=1
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
